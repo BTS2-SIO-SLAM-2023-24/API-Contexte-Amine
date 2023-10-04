63e2fef2d17000f3bd064c13868c2a2cb57033e3
amineb213 <bensadi.amine18@gmail.com>
2023-10-04 10:08:38 +0200

    update

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.dqaxuyi.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp
 `;
 
-const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000;
+const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 27017;
 
 export const config = {
     mongo: {
